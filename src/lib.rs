8f55ba4d9d78f874c4d3f5735b8212275049d0af
Patrick Lorio <patrick@methods.dev>
2024-12-29 13:01:07 -0800

    Trim path on last src

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub use spans::{Span, SpanBuilder, set_min_log_level, disable_logging};
 pub use logs::Log;
 
 pub(crate) fn trim_src_path(path: &str) -> &str {
-    if let Some((_, result)) = path.split_once("/src/") {
+    if let Some((_, result)) = path.rsplit_once("/src/") {
         return result;
     }
     if let Some(result) = path.strip_prefix("src/") {
